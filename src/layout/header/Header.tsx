c5b9d9b46cb4f73f11094730871662faeb28c029
mykytos <kytyzovns@gmail.com>
2024-05-24 12:40:16 +0300

    created basic structure, added header

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components";
+import {Logo} from "../../components/logo/Logo";
+import {Menu} from "../../components/menu/Menu";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo iconId="logo" width="65px" height="48px" viewBox="0 0 65 48"/>
+            <Menu/>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+    background-color: aquamarine;
+    display: flex;
+    justify-content: center;
+    gap: 100px;
+`
