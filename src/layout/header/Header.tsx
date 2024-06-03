afb601111fe3b21161815dd29bc438b38596b85c
Vladimir <seryyvladimir@gmail.com>
2024-06-03 22:32:59 +0300

    refining header styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,20 +2,30 @@ import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
+import {HeaderMenu} from "./HeaderMenu";
+import {theme} from "../../styles/Theme";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
 
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo iconId="logo" width="65px" height="48px" viewBox="0 0 65 48"/>
-            <Menu direction={"row"} gap={"80px"}/>
+            <Container>
+                <FlexWrapper justify={"center"} align={"center"} gap={"100px"}>
+                    <Logo iconId="logo" width="65px" height="48px" viewBox="0 0 65 48"/>
+                    <HeaderMenu items={["Contacts", "About me", "Portfolio", "Links"]}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    background-color: aquamarine;
-    display: flex;
-    justify-content: center;
-    gap: 100px;
+    background-image: ${theme.colors.primaryBg};
+    position: fixed;
+    top:0;
+    left:0;
+    right:0;
+    z-index: 99999;
 `
---
+++ src/layout/header/HeaderMenu.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from "styled-components";
+import {theme} from "../../styles/Theme";
+
+type MenuProps = {
+    items: Array<string>;
+}
+
+export const HeaderMenu = (props: MenuProps) => {
+    return (
+        <StyledNav>
+            <ul>
+                {props.items.map((item, index) => {
+                    return (
+                        <Item key={index}>
+                            <Link>
+                                {item}
+                                <Mask>
+                                    <span>{item}</span>
+                                </Mask>
+                                <Mask>
+                                    <span>{item}</span>
+                                </Mask>
+                            </Link>
+                        </Item>
+                    );
+                })}
+            </ul>
+        </StyledNav>
+    );
+};
+
+
+const StyledNav = styled.nav`
+    ul {
+        display: flex;
+        list-style: none;
+        gap: 60px;
+    }
+`
+
+const Mask = styled.span`
+    color: ${theme.colors.primaryTextColor};
+    position: absolute;
+    height: 50%;
+    top: 0;
+    left: 0;
+    overflow-y: hidden;
+    display: inline-block;
+    & + & {
+        top: 50%;
+        span{
+            display: inline-block;
+            transform: translateY(-50%);
+        }
+    }
+`
+
+const Link = styled.a`
+    color: transparent;
+    font-size: 18px;
+`
+
+const Item = styled.li`
+    position: relative;
+
+    &::before {
+        content: "";
+        height: 2px;
+        position: absolute;
+        top: 50%;
+        left: -10px;
+        right: -10px;
+        background-color: ${theme.colors.linkTextColor};
+        z-index: 1;
+        transform: scale(0);
+    }
+
+    &:hover {
+        &::before {
+            transform: scale(1);
+        }
+
+        ${Mask} {
+            transform: skewX(12deg) translateX(5px);
+        }
+
+        ${Mask} + ${Mask} {
+            transform: skewX(12deg) translateX(-5px);
+        }
+    }
+`
